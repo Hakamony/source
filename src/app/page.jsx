9dea432835562102367ca4db547de788aba41c8c
Moath-COE <moathabdelaziz@outlook.com>
2024-08-04 18:00:23 +0300

    Add fav icon to the project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,11 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<main>
-			<h1>This is the home page.</h1>
-			<Link href="/SelectEvent">Select Event Page</Link>
+		<main className="flex items-center flex-col w-[100vw] h-[100vh] justify-center">
+			<h1 className="text-4xl font-bold">This is the home page.</h1>
+			<Link href="/SelectEvent" className="text-blue-700">
+				Select Event Page
+			</Link>
 		</main>
 	);
 }
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+	return (
+		<nav>
+			<li>Home</li>
+			<li>Past Events</li>
+			<li>Create new</li>
+		</nav>
+	);
+}
