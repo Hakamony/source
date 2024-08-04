485b064d1ce2d264bf32845cd819e9d3df2495a1
Moath-COE <moathabdelaziz@outlook.com>
2024-08-04 17:13:41 +0300

    Add pages structure

---
+++ src/app/SelectEvent/page.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function SelectEvent() {
+	return (
+		<main>
+			<h1>This is the select event page.</h1>
+			<Link href="/">Back Home</Link>
+		</main>
+	);
+}
