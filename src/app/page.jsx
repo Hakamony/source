c55f7ff78aad217a5485b0d8f2c6691121b25898
Moath-COE <moathabdelaziz@outlook.com>
2024-08-04 23:37:38 +0300

    Setup objects used in the project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export default function Home() {
 	return (
 		<main className="flex items-center flex-col w-[100vw] h-[100vh] justify-center">
-			<h1 className="text-4xl font-bold">This is the home page.</h1>
+			<h1 className="text-4xl font-bold border-black border-2 mb-10">
+				Welcome to Hakamony
+			</h1>
 			<Link href="/SelectEvent" className="text-blue-700">
 				Select Event Page
 			</Link>
