c8a72d492c472d34e1e7c63898dbe4a95c5065be
Moath-COE <moathabdelaziz@outlook.com>
2024-08-09 20:12:09 +0300

    Add design colors to tailwind config file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<main className="flex items-center flex-col w-[100vw] h-[100vh] justify-center">
-			<h1 className="text-4xl font-bold border-black border-2 mb-10">
+		<main className="flex items-center flex-col w-[100vw] h-[100vh] justify-center bg-prime-white">
+			<h1 className="text-4xl font-bold border-prime-orange border-2 mb-10 text-prime-blue">
 				Welcome to Hakamony
 			</h1>
-			<Link href="/SelectEvent" className="text-blue-700">
+			<Link href="/SelectEvent" className="text-prime-dark">
 				Select Event Page
 			</Link>
 		</main>
