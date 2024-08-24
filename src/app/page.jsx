f527cb9fd2d30f62716ae02c70c0f1318c04f59e
Moath Abdelaziz <93261842+Moath-COE@users.noreply.github.com>
2024-08-24 22:32:04 +0300

    Setup home page initially

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,21 @@
 import Image from 'next/image';
-import hakamImg from '../../public/assets/hakamDraft.webp';
+import hakamImg from '../../public/assets/hakamDraft.png';
 import Button from '@/components/layout/ButtonNav';
 
 export default function Home() {
 	return (
-		<main className="flex h-screen w-screen flex-col items-center justify-center font-main">
+		<main className="h-100vh flex h-screen w-screen flex-col items-center justify-between py-8 font-main">
 			<section>
-				<Image src={hakamImg} alt="Picture of the author" width={500} />
+				<Image src={hakamImg} alt="Picture of the author" width={300} />
 			</section>
 			<section className="flex flex-col gap-4 text-center">
 				<Button link="/SelectEvent" color="orange">
 					ابدأ الفاعلية
 				</Button>
-				<Button link="/SelectEvent" color="orange">
+				<Button link="/PtrvEvents" color="orange">
 					عرض الفاعليات
 				</Button>
-				<Button link="/SelectEvent" color="orange">
+				<Button link="/RandomGames" color="orange">
 					العاب عشوائية
 				</Button>
 			</section>
