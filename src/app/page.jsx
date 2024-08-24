5310fba7dc3a79f3f86dc3e9762b5a6a4dc2b0af
Moath-COE <moathabdelaziz@outlook.com>
2024-08-24 20:13:26 +0300

    Setup main font family

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<main className="flex h-screen w-screen flex-col items-center justify-center">
+		<main className="font-main flex h-screen w-screen flex-col items-center justify-center">
 			<h1 className="mb-10 border-2 border-prime-orange text-4xl font-bold text-prime-blue">
 				Welcome to Hakamony
 			</h1>
