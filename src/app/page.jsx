953f4d6d5912a9c1bb60abbd8f78f83b9f268f30
Moath-COE <moathabdelaziz@outlook.com>
2024-08-24 21:09:53 +0300

    Move to windows

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,24 @@
+import Image from 'next/image';
+import hakamImg from '../../public/assets/hakamDraft.webp';
 import Button from '@/components/layout/ButtonNav';
 
 export default function Home() {
 	return (
-		<main className="font-main flex h-screen w-screen flex-col items-center justify-center">
-			<Button link="/SelectEvent" color="orange">
-				ابدأ الفاعلية
-			</Button>
+		<main className="flex h-screen w-screen flex-col items-center justify-center font-main">
+			<section>
+				<Image src={hakamImg} alt="Picture of the author" width={500} />
+			</section>
+			<section className="flex flex-col gap-4 text-center">
+				<Button link="/SelectEvent" color="orange">
+					ابدأ الفاعلية
+				</Button>
+				<Button link="/SelectEvent" color="orange">
+					عرض الفاعليات
+				</Button>
+				<Button link="/SelectEvent" color="orange">
+					العاب عشوائية
+				</Button>
+			</section>
 		</main>
 	);
 }
--- src/components/layout/ButtonNav.jsx
+++ src/components/layout/ButtonNav.jsx
@@ -4,7 +4,7 @@ export default function ButtonDev({ children, ...props }) {
 	return (
 		<button
 			type="submit"
-			className={`rounded-lg bg-prime-${props.color} px-20 py-2 text-prime-white`}
+			className={`rounded-lg bg-prime-${props.color} px-20 py-2 text-xl font-bold text-prime-white`}
 		>
 			<Link href={props.link}>{children}</Link>
 		</button>
