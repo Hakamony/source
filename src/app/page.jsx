9194b22186cb924f5464a0800c8e7db7b9bbf1aa
Moath-COE <moathabdelaziz@outlook.com>
2024-08-24 20:03:27 +0300

    Create ButtonDev component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,11 @@
-import Link from 'next/link';
+import Button from '@/components/layout/ButtonNav';
 
 export default function Home() {
 	return (
 		<main className="flex h-screen w-screen flex-col items-center justify-center">
-			<h1 className="mb-10 border-2 border-prime-orange text-4xl font-bold text-prime-blue">
-				Welcome to Hakamony
-			</h1>
-			<Link href="/SelectEvent" className="text-prime-dark">
-				Select Event Page
-			</Link>
+			<Button link="/SelectEvent" color="orange">
+				ابدأ الفاعلية
+			</Button>
 		</main>
 	);
 }
