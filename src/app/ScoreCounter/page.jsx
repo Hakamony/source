3c2186b1086b75c53360ed41a3982fd1a8c9a9b8
Yousef Mahmoud <mryoyo133@gmail.com>
2024-08-30 01:41:49 +0300

    update

--- src/app/ScoreCounter/page.jsx
+++ src/app/ScoreCounter/page.jsx
@@ -2,9 +2,9 @@ import Counter from "@/components/input/Counter";
 
 export default function ScoreCounterPage() {
 	return (
-		<main className="font-main h-full px-4 py-12">
+		<main className="font-main h-[80vh] px-4 py-12">
 			<h1 className="text-center text-4xl font-bold mb-2">حاسبة النقاط</h1>
-            <div className=" flex-col flex justify-between h-full">
+            <div className=" flex-col flex justify-around h-full">
                 <h2 className="text-center text-2xl">الفريق أ</h2>
                 <Counter/>
                 <h2 className="text-center text-2xl">الفريق ب</h2>
--- src/components/input/Counter.jsx
+++ src/components/input/Counter.jsx
@@ -15,9 +15,9 @@ function Counter() {
     }
 
 	return (
-		<div className={'flex mx-2 items-center h-24'}>
+		<div className={'flex mx-2 items-center h-1/5'}>
 			<button onClick = {handlePlus} className={`w-1/5 rounded-r-lg h-full bg-prime-orange text-center text-4xl font-bold text-prime-white`}>+</button>
-            <div className={"w-3/5 bg-gray-500 text-white flex items-center justify-center text-center text-4xl h-full"}>{num}</div>
+            <div className={"w-3/5 bg-gray-500 text-white flex items-center justify-center text-center text-6xl h-full"}>{num}</div>
 			<button onClick = {handleMinus} className={`w-1/5 items-center rounded-l-lg h-full bg-prime-orange text-center text-4xl font-bold text-prime-white`}>-</button>
 		</div>
 	);
