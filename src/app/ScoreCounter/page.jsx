02eff7f9b2d4c272efc4bd63f12a45d1dd15e520
Yousef Mahmoud <mryoyo133@gmail.com>
2024-08-30 01:44:32 +0300

    update 2

--- src/app/ScoreCounter/page.jsx
+++ src/app/ScoreCounter/page.jsx
@@ -4,7 +4,7 @@ export default function ScoreCounterPage() {
 	return (
 		<main className="font-main h-[80vh] px-4 py-12">
 			<h1 className="text-center text-4xl font-bold mb-2">حاسبة النقاط</h1>
-            <div className=" flex-col flex justify-around h-full">
+            <div className=" flex-col flex justify-around h-full max-w-lg">
                 <h2 className="text-center text-2xl">الفريق أ</h2>
                 <Counter/>
                 <h2 className="text-center text-2xl">الفريق ب</h2>
