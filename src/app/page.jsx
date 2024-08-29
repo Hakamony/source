62620296d139d6af70e79c5da83ef60da8b67d56
Yousef Mahmoud <mryoyo133@gmail.com>
2024-08-29 22:13:41 +0300

    eslint

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Storage from '@/components/layout/Storage';
 
 export default function Home() {
 
@@ -8,7 +7,6 @@ export default function Home() {
 			<h1 className="mb-10 border-2 border-prime-orange text-4xl font-bold text-prime-blue">
 				Welcome to Hakamony
 			</h1>
-			<Storage/>
 			<Link href="/SelectEvent" className="text-prime-dark">
 				Select Event Page
 			</Link>
