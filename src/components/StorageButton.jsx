2df9f95321a37574d004507bf5c08f06b04b6d76
Yousef Mahmoud <mryoyo133@gmail.com>
2024-09-18 17:30:50 +0300

    testing the getNextMatch function

--- src/components/StorageButton.jsx
+++ src/components/StorageButton.jsx
@@ -3,10 +3,26 @@
 import eventStorage from '@/lib/storage/eventStorage';
 import matchStorage from '@/lib/storage/matchStorage';
 import pastEventStorage from '@/lib/storage/pastEventStorage';
+import teamStorage from '@/lib/storage/teamStorage';
 
 export default function StorageButton() {
 	const handleButton = () => {
-		console.log(matchStorage.getNextMatch());
+		const nextMatchId = matchStorage.getNextMatch();
+		const matchList = matchStorage.getMatchesList()
+
+		matchStorage.updateMatch(matchList[0], {status: 1});
+		matchStorage.updateMatch(matchList[1], {status: 1});
+		matchStorage.updateMatch(matchList[5], {status: 1});
+
+
+		// for(let i =0; i < matchList.length; i++){
+		// 	matchStorage.updateMatch(matchList[i], {status: 0});
+		// }
+		const position = matchList.indexOf(nextMatchId);
+		const match = matchStorage.getMatch(nextMatchId);
+
+		console.log(position);
+		console.log(match);
 	};
 
 	return (
